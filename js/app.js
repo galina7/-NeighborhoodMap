771162eea9c97a3b0c5bb027dd437db323a1dc99
Galina Ilchenco <GIlchenco@bills.com>
2016-10-21 10:37:17 -0700

    corrections after review1

--- js/app.js
+++ js/app.js
@@ -1,81 +1,95 @@
 "use strict"
 
-     var map,
-          marker,
-          infoWindow;
+var map,
+    marker,
+    infoWindow;
 
-     var availableGalleries = [
-          {
-            title: 'Anno Domini', 
-            location: {lat: 37.3307, lng: -121.886},
-            img: "img/AnnoDomini.png",
-            address: "366 S 1st St",
-            phone: "(408) 271-5155",
-            web: "http://www.galleryad.com/"
-          },
-          {
-            title: 'Kaleid Gallery', 
-            location: {lat: 37.336506, lng: -121.886143},
-            img: "img/Kaleid.png",
-            address: "88 S 4th St",
-            phone: "(408) 271-5151",
-            web: "http://www.kaleidgallery.com",
-          },
-          {
-            title: 'Art Object', 
-            location: {lat: 37.348799, lng: -121.893726},
-            img: "img/ArtObject.png",
-            address: "592 N 5th St",
-            phone: "(408) 288-9305",
-            web: "http://www.artobjectgallery.com",
-          },
-          {
-            title: 'Works', 
-            location: {lat: 37.329155, lng: -121.888096},
-            img: "img/works.png",
-            address: "365 S Market St",
-            phone: "(408) 300-6405",
-            web: "http://www.workssanjose.org",
-          },
-          {
-            title: 'Higher Fire Clayspace & Gallery', 
-            location: {lat: 37.328162, lng: -121.885733},
-            img: "img/HigherFire.png",
-            address: "499 S Market St",
-            phone: "(408) 295-5765",
-            web: "http://www.higherfirestudios.com",
-          },
-          {
-            title: 'Aperture Academy', 
-            location: {lat: 37.378754, lng: -121.921401},
-            img: "img/ApertureAcademy.png",
-            address: "2290 N. 1st Street",
-            phone: "(408) 369-8585",
-            web: "http://www.apertureacademy.com",
-          }
-    ];
+var availableGalleries = [{
+    title: 'Anno Domini',
+    location: {
+        lat: 37.3307,
+        lng: -121.886
+    },
+    img: "img/AnnoDomini.png",
+    address: "366 S 1st St",
+    phone: "(408) 271-5155",
+    web: "http://www.galleryad.com/"
+}, {
+    title: 'Kaleid Gallery',
+    location: {
+        lat: 37.336506,
+        lng: -121.886143
+    },
+    img: "img/Kaleid.png",
+    address: "88 S 4th St",
+    phone: "(408) 271-5151",
+    web: "http://www.kaleidgallery.com",
+}, {
+    title: 'Art Object',
+    location: {
+        lat: 37.348799,
+        lng: -121.893726
+    },
+    img: "img/ArtObject.png",
+    address: "592 N 5th St",
+    phone: "(408) 288-9305",
+    web: "http://www.artobjectgallery.com",
+}, {
+    title: 'Works',
+    location: {
+        lat: 37.329155,
+        lng: -121.888096
+    },
+    img: "img/works.png",
+    address: "365 S Market St",
+    phone: "(408) 300-6405",
+    web: "http://www.workssanjose.org",
+}, {
+    title: 'Higher Fire Clayspace & Gallery',
+    location: {
+        lat: 37.328162,
+        lng: -121.885733
+    },
+    img: "img/HigherFire.png",
+    address: "499 S Market St",
+    phone: "(408) 295-5765",
+    web: "http://www.higherfirestudios.com",
+}, {
+    title: 'Aperture Academy',
+    location: {
+        lat: 37.378754,
+        lng: -121.921401
+    },
+    img: "img/ApertureAcademy.png",
+    address: "2290 N. 1st Street",
+    phone: "(408) 369-8585",
+    web: "http://www.apertureacademy.com",
+}];
 
 
 
 
 function initMap() {
 
-       map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.338208, lng: -121.886329},
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 37.338208,
+            lng: -121.886329
+        },
         zoom: 15
-      });
+    });
 
-      infoWindow = new google.maps.InfoWindow();
+    infoWindow = new google.maps.InfoWindow();
 
     //create markers
     for (var i = 0; i < availableGalleries.length; i++) {
-          var position = availableGalleries[i].location;
-          var title = availableGalleries[i].title;
-          var address = availableGalleries[i].address;
-          var phone = availableGalleries[i].phone;
-          var web = availableGalleries[i].web;
-          var img = availableGalleries[i].img;
-          marker = new google.maps.Marker({
+        var position = availableGalleries[i].location;
+        var title = availableGalleries[i].title;
+        var address = availableGalleries[i].address;
+        var phone = availableGalleries[i].phone;
+        var web = availableGalleries[i].web;
+        var img = availableGalleries[i].img;
+        marker = new google.maps.Marker({
             map: map,
             position: position,
             title: title,
@@ -86,54 +100,55 @@ function initMap() {
             animation: google.maps.Animation.DROP,
             info: availableGalleries[i].infoWindow,
             img: availableGalleries[i].img,
-          });
-          availableGalleries[i].markerObject = marker;
-          marker.addListener('click', function() {
+        });
+        availableGalleries[i].markerObject = marker;
+        marker.addListener('click', function() {
             popInfoWindow(this, infoWindow);
-          });
-      }
- };   
+        });
+    }
+};
 
 
-    //creat infoWindow
-    function popInfoWindow(marker, infoWindow) {
+//creat infoWindow
+function popInfoWindow(marker, infoWindow) {
 
-      var marker = marker;
-      var wikiUrl =  'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
-      var wikiRequestTimeout = setTimeout(function() {
-            alert("Failed to get wikipedia resources");
-        },8000);
+    var marker = marker;
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function() {
+        alert("Failed to get wikipedia resources");
+    }, 8000);
 
-      $.ajax({
+    $.ajax({
         url: wikiUrl,
         dataType: "jsonp",
         // ajax settings
-            success: function (response) {
-                var wikiStr = response[0];
-                var url = 'http://en.wikipedia.org/wiki/' + wikiStr;
+        success: function(response) {
+            var wikiStr = response[0];
+            var url = 'http://en.wikipedia.org/wiki/' + wikiStr;
 
-                if (infoWindow.marker != marker) {
-                  infoWindow.marker = marker;
-                  marker.addListener('click', toggleBounce(marker));
-                  infoWindow.setContent("<div><img src='"+marker.img+"'/><br /></div><div class='info-title'>"+marker.title+"</div><br /><p class='address'>"+marker.address+"<br />"+marker.phone+"<br />Wiki Page:<br /><a href='"+url+"'>"+url+"</a></p>");
-                  infoWindow.open(map, marker);
-                  infoWindow.addListener("closeclick", function() {
-                  });
-                }
-                clearTimeout(wikiRequestTimeout);
+            if (infoWindow.marker != marker) {
+                infoWindow.marker = marker;
+                marker.addListener('click', toggleBounce(marker));
+                infoWindow.setContent("<div><img src='" + marker.img + "'/><br /></div><div class='info-title'>" + marker.title + "</div><br /><p class='address'>" + marker.address + "<br />" + marker.phone + "<br />Wiki Page:<br /><a href='" + url + "'>" + url + "</a></p>");
+                infoWindow.open(map, marker);
+                infoWindow.addListener("closeclick", function() {});
             }
-        });
-    }
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+}
 
 //Got from https://developers.google.com/maps/documentation/javascript/examples/marker-animations
-    function toggleBounce(marker) {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout(function(){ marker.setAnimation(null); }, 750);
-        }
-      }
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 750);
+    }
+}
 
 
 
@@ -147,10 +162,10 @@ var AppViewModel = function() {
 
     //click on gallery to display marker
     self.clickGallery = function(availableGalleries) {
-      map.setZoom (18);
-      map.setCenter(availableGalleries.location);
+        map.setZoom(18);
+        map.setCenter(availableGalleries.location);
 
-      popInfoWindow(availableGalleries.markerObject, infoWindow);
+        popInfoWindow(availableGalleries.markerObject, infoWindow);
     };
 
 
@@ -163,23 +178,23 @@ var AppViewModel = function() {
     //     });
     //   });
 
-        self.search = ko.computed(function() {
+    self.search = ko.computed(function() {
         // Got lines 51-53 from https://discussions.udacity.com/t/search-function-implemetation/15105/33
-         var newArray = ko.utils.arrayFilter(self.availableGalleries(), function(gallery)  {
-              if(gallery.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
-                if(gallery.marker) {
-                    gallery.marker.setVisible(true);
+        var newArray = ko.utils.arrayFilter(self.availableGalleries(), function(gallery) {
+            if (gallery.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+                if (gallery.markerObject) {
+                    gallery.markerObject.setVisible(true);
                 }
                 return true;
-              }else {
-                if(gallery.marker) {
-                    gallery.marker.setVisible(false);
+            } else {
+                if (gallery.markerObject) {
+                    gallery.markerObject.setVisible(false);
                 }
                 return false;
-              }
+            }
         });
-         return newArray;
-      });
+        return newArray;
+    });
 
 };
 
